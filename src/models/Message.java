af57a3db8e4df28203a4e463a9fafb6831c1c812
shinnosuke.ono <nyz549ir@gmail.com>
2022-04-19 18:32:33 +0900

    getAllMessagesを追加

--- src/models/Message.java
+++ src/models/Message.java
@@ -7,9 +7,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+            name="getAllMessages",
+            query="SELECT m FROM Message AS m ORDER BY m.id DESC"
+            )
+})
 @Table(name="messages")
 public class Message {
     @Id
@@ -23,7 +31,7 @@ public class Message {
     @Column(name="content",length=255,nullable=false)
     private String content;
 
-    @Column(name="created_at,nullable=false")
+    @Column(name="created_at",nullable=false)
     private Timestamp created_at;
 
     @Column(name="updated_at",nullable=false)
